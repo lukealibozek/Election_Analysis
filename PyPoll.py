68c98692b1602384705c112e468c193ef5c59c49
lukealibozek-cAXrSb <lukealibozek@gmail.com>
2021-12-26 15:19:29 -0600

    starting 3.4.5

--- PyPoll.py
+++ PyPoll.py
@@ -7,9 +7,10 @@ import os
 # 4. The total number of votes each vandidate won
 # 5. The winner of the election based on popular vote
 
-cwd = os.getcwd()
-files = os.listdir(cwd)
-print(f"Files in {cwd}: {files}")
+# Check current working directory - troubleshooting VS Code
+# cwd = os.getcwd()
+# files = os.listdir(cwd)
+# print(f"Files in {cwd}: {files}")
 
 # Assign a variable for the file to load and the path.
 file_to_load = os.path.join('Resources','election_results.csv')
@@ -17,8 +18,23 @@ file_to_load = os.path.join('Resources','election_results.csv')
 #Open the election results and read the file.
 with open(file_to_load) as election_data:
 
-    # To do: perform analysis
-    print(election_data)
+    # To do: read and analyze the data here
+    file_reader = csv.reader(election_data)
+    
+    # Print the header row
+    headers = next(file_reader)
+    print(headers)
+    input("next?")
+
+    headers = next(file_reader)
+    print(headers)
+    input("next?")
+
+    #Print each row in the CSV file
+    for row in file_reader:
+        print(row[1])
+
+        input("next?")
 
 #Close the file.
 election_data.close()
