9f201fc81e185311f6c05502c217181e995c898a
lukealibozek-cAXrSb <lukealibozek@gmail.com>
2021-12-26 14:51:31 -0600

    troubleshooting VS code relative path issues

--- PyPoll.py
+++ PyPoll.py
@@ -7,6 +7,10 @@ import os
 # 4. The total number of votes each vandidate won
 # 5. The winner of the election based on popular vote
 
+cwd = os.getcwd()
+files = os.listdir(cwd)
+print(f"Files in {cwd}: {files}")
+
 # Assign a variable for the file to load and the path.
 file_to_load = os.path.join('Resources','election_results.csv')
 
@@ -19,14 +23,14 @@ with open(file_to_load) as election_data:
 #Close the file.
 election_data.close()
 
-# Create a filename variable to a direct or indirect path to the file.
-file_to_save = os.path.join("analysis", "election_analysis.txt")
+# # Create a filename variable to a direct or indirect path to the file.
+# file_to_save = os.path.join("analysis", "election_analysis.txt")
 
-# Using the with statement open the file as a text file
-with open(file_to_save, "w") as txt_file:
-    #Write some data
-    txt_file.write("Counties in the Election\n--------------------\n")
-    txt_file.write("Arapahoe\nDenver\nJefferson")
+# # Using the with statement open the file as a text file
+# with open(file_to_save, "w") as txt_file:
+#     #Write some data
+#     txt_file.write("Counties in the Election\n--------------------\n")
+#     txt_file.write("Arapahoe\nDenver\nJefferson")
 
-#Close the file
-#.close()
+# #Close the file
+# .close()
